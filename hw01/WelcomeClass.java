93829466a0579b462e319a63f6ef7e3b24b59c03
kaeli107 <kmp218@lehigh.edu>
2014-08-30 21:26:40 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////
+//Kaeli Prevelige
+//Homework 1
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+
+//  add main method
+  public static void main(String[] args) {
+      
+//  print the statement with the box around it
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-K--M--P--2--1--8-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This August, my friends and I took a one-day,");
+    System.out.println("fifteen mile hike on the Appalachian Trail in CT.");
+
+    //test/testtest
+  }
+}
